ba1104d92b8c3f7b5a13dcaa2d15a152b7de1144
epoch <epoch@hack.thebackupbox.net>
2020-03-28 05:07:58 +0000

    ipconvert can now do IPv6 addresses too.

--- ipconvert.c
+++ ipconvert.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 int main(int argc,char *argv[]) {
- int addr=inet_addr(argv[1]);
- printf("hex: %x\n",htonl(addr));
- printf("dec: %u\n",htonl(addr));
- printf("dotted-quad: %u.%u.%u.%u\n",addr&255,addr>>8&255,addr>>16&255,addr>>24&255);
+ struct in6_addr in6_addr;
+ char in6s[40];// 8 sections, 4 bytes each, 7 1byte separators, 1 null byte. = 40
+ unsigned int addr;
+ if(strchr(argv[1],':')) {
+  inet_pton(AF_INET6,argv[1],&in6_addr);
+  inet_ntop(AF_INET6,&in6_addr,in6s,sizeof(in6s));
+  printf("%s\n",in6s);
+ } else {
+  inet_pton(AF_INET,argv[1],&addr);
+  printf("hex: %x\n",htonl(addr));
+  printf("dec: %u\n",htonl(addr));
+  printf("dotted-quad: %u.%u.%u.%u\n",addr&255,addr>>8&255,addr>>16&255,addr>>24&255);
+ }
  return 0;
 }
